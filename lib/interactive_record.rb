b41e9e49411e50a3d203fa98d17e09d8399856a2
Dmitri Samotessov <dimasamo96@gmail.com>
2019-02-23 00:39:44 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,11 +1,21 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-require 'pry'
 
 class InteractiveRecord
-
   def self.table_name
-    self.to_s.downcase.pluralize
+    "#{self.to_s.downcase}s"
+  end
+
+  def self.column_names
+
+    sql = "pragma table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
   end
 
   def initialize(options={})
@@ -14,27 +24,14 @@ class InteractiveRecord
     end
   end
 
-  def self.column_names
-    sql = <<-SQL
-        PRAGMA table_info("#{table_name}")
-    SQL
-
-    table_data = DB[:conn].execute(sql)
-    column_names = []
-    table_data.each do |hash|
-        hash.each do |key, value|
-        column_names.push(value) if key.to_s == "name"
-      end
-    end
-    column_names
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
   def table_name_for_insert
     self.class.table_name
-  end
-
-  def col_names_for_insert
-    self.class.column_names.delete_if {|element| element=="id"}.join(", ")
   end
 
   def values_for_insert
@@ -45,35 +42,19 @@ class InteractiveRecord
     values.join(", ")
   end
 
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+
   def self.find_by_name(name)
-   sql = <<-SQL
-     SELECT * FROM #{table_name}
-     WHERE name = ?
-     SQL
-
-   DB[:conn].execute(sql, name);
-  end
-
-  def self.find_by(attribute)
-    column_name = attribute.keys[0].to_s
-    value_name = attribute.values[0]
-
-    sql = <<-SQL
-      SELECT * FROM #{table_name}
-      WHERE #{column_name} = ?
-      SQL
-
-    DB[:conn].execute(sql, value_name);
-  end
-
-
-  def save
-    sql = <<-SQL
-      INSERT INTO #{table_name_for_insert}(#{col_names_for_insert})
-      VALUES (#{values_for_insert})
-      SQL
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
+  def self.find_by(attribute_hash)
+    value = attribute_hash.values.first
+    formatted_value = value.class == Fixnum ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+    DB[:conn].execute(sql)
+  end
 end
